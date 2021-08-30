c385c1962a07f088b07cd78c04d51f09a8fd1973
slhs92004 <slhs92004@gmail.com>
2021-08-30 19:34:30 +0800

    move /_04_productPage
    @RequestMapping("/_04_productPage") 改到BuyProductPageController

--- src/main/java/com/web/store/controller/BuyMenuController.java
+++ src/main/java/com/web/store/controller/BuyMenuController.java
@@ -44,13 +44,13 @@ public class BuyMenuController {
 		return "_04_buyProductMenu";
 	}
 
-	@RequestMapping("/_04_productPage")
-	public String buyProduct(Model model) {
-		List<ProductBean> products = productService.getAllProducts();
-		List<ProductTypeBean> productTypes = productService.getAllProdTypes();
-		model.addAttribute("products", products);
-		model.addAttribute("productTypes", productTypes);
-		return "_04_productPage";
-	}
+//	@RequestMapping("/_04_productPage") ---------------改到BuyProductPageController
+//	public String buyProduct(Model model) {
+//		List<ProductBean> products = productService.getAllProducts();
+//		List<ProductTypeBean> productTypes = productService.getAllProdTypes();
+//		model.addAttribute("products", products);
+//		model.addAttribute("productTypes", productTypes);
+//		return "_04_productPage";
+//	}
 	
 }
