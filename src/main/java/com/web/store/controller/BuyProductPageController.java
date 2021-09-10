d2dbcf0fdbfd4a2c6bc61263f5a13a486b6cda23
iris <iris22iris@gmail.com>
2021-09-10 17:46:07 +0800

    評價傳回會員圖片 完成

--- src/main/java/com/web/store/controller/BuyProductPageController.java
+++ src/main/java/com/web/store/controller/BuyProductPageController.java
@@ -33,11 +33,11 @@ import com.web.store.service.ProductService;
 @Controller
 public class BuyProductPageController {
 
-//	@Autowired
-//	ServletContext context;
-//
-//	@Autowired
-//	CustomerService customerService;
+	@Autowired
+	ServletContext context;
+
+	@Autowired
+	CustomerService customerService;
 
 	ProductService productService;
 	ServletContext servletContext;
@@ -105,71 +105,71 @@ public class BuyProductPageController {
 //	}
 	
 	//傳回會員圖片
-//	@GetMapping("/getMemberImg")
-//	public ResponseEntity<byte[]> getMemberImg(@RequestParam("custId") Integer custId) {
-//		InputStream is = null;
-//		OutputStream os = null;
-//		String fileName = null;
-//		String mimeType = null;
-//		byte[] media = null;
-//		ResponseEntity<byte[]> responseEntity = null;
-//		HttpHeaders headers = new HttpHeaders();
-//		MediaType mediaType = null;
-//		Blob blob = null;
-//		try {
-//			CustomerBean bean = customerService.getCustomerById(custId);
-//			if (bean != null) {
-//				blob = bean.getCustomerImage();
-//				if (blob != null) {
-//					is = blob.getBinaryStream();
-//				}
-//				fileName = bean.getFileName();
-//			}
-//			// 如果圖片的來源有問題，就送回預設圖片(/images/NoImage.png)
-//			if (is == null) {
-//				fileName = "member.jpg";
-//				is = context.getResourceAsStream("/images/" + fileName);
-//			}
-//
-//			// 由圖片檔的檔名來得到檔案的MIME型態
-//			mimeType = context.getMimeType(fileName);
-//			if (mimeType == null) {
-//				if (fileName.endsWith("jfif")) {
-//					mimeType = "image/jfif";
-//				}
-//			}
-//			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//			// 由InputStream讀取位元組，然後由OutputStream寫出
-//			int len = 0;
-//			byte[] bytes = new byte[8192];
-//
-//			while ((len = is.read(bytes)) != -1) {
-//				baos.write(bytes, 0, len);
-//			}
-//			media = baos.toByteArray();
-//			mediaType = MediaType.valueOf(mimeType);
-//			headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-//			headers.setContentType(mediaType);
-//			responseEntity = new ResponseEntity<>(media, headers, HttpStatus.OK);
-//
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//			throw new RuntimeException("/getMemberImg#doGet()發生Exception: " + ex.getMessage());
-//		} finally {
-//			try {
-//				if (is != null)
-//					is.close();
-//			} catch (IOException e) {
-//				;
-//			}
-//			try {
-//				if (os != null)
-//					os.close();
-//			} catch (IOException e) {
-//				;
-//			}
-//		}
-//		return responseEntity;
-//	}
+	@GetMapping("/_04_productPage/getMemberImg")
+	public ResponseEntity<byte[]> getMemberImg(@RequestParam("custId") Integer custId) {
+		InputStream is = null;
+		OutputStream os = null;
+		String fileName = null;
+		String mimeType = null;
+		byte[] media = null;
+		ResponseEntity<byte[]> responseEntity = null;
+		HttpHeaders headers = new HttpHeaders();
+		MediaType mediaType = null;
+		Blob blob = null;
+		try {
+			CustomerBean bean = customerService.getCustomerById(custId);
+			if (bean != null) {
+				blob = bean.getCustomerImage();
+				if (blob != null) {
+					is = blob.getBinaryStream();
+				}
+				fileName = bean.getFileName();
+			}
+			// 如果圖片的來源有問題，就送回預設圖片(/images/NoImage.png)
+			if (is == null) {
+				fileName = "member.jpg";
+				is = context.getResourceAsStream("/images/" + fileName);
+			}
+
+			// 由圖片檔的檔名來得到檔案的MIME型態
+			mimeType = context.getMimeType(fileName);
+			if (mimeType == null) {
+				if (fileName.endsWith("jfif")) {
+					mimeType = "image/jfif";
+				}
+			}
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			// 由InputStream讀取位元組，然後由OutputStream寫出
+			int len = 0;
+			byte[] bytes = new byte[8192];
+
+			while ((len = is.read(bytes)) != -1) {
+				baos.write(bytes, 0, len);
+			}
+			media = baos.toByteArray();
+			mediaType = MediaType.valueOf(mimeType);
+			headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+			headers.setContentType(mediaType);
+			responseEntity = new ResponseEntity<>(media, headers, HttpStatus.OK);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new RuntimeException("/getMemberImg#doGet()發生Exception: " + ex.getMessage());
+		} finally {
+			try {
+				if (is != null)
+					is.close();
+			} catch (IOException e) {
+				;
+			}
+			try {
+				if (os != null)
+					os.close();
+			} catch (IOException e) {
+				;
+			}
+		}
+		return responseEntity;
+	}
 
 }
