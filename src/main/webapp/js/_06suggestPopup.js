0028ac68912251e6b18316551697c3f7121d58b6
jimmy <a501302001@gmail.com>
2021-09-13 20:37:23 +0800

    add suggestpopup.jsp

---
+++ src/main/webapp/js/_06suggestPopup.js
@@ -0,0 +1,42 @@
+
+document.getElementById("contentpopup").innerHTML = 
+''+
+'    <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">'+
+'        <div class="modal-dialog col-12">'+
+'               <div class="modal-content">'+
+'                    <h2 class="modal-title1 p-3 col-12 d-flex justify-content-evenly " id="exampleModalLabel">新品更優惠</h2>'+                                                                
+'                        <div class="containerPOPUP p-3  ">'+
+'                            <form class="row g-3 form">'+
+'                                <div class="col-12 justify-content-evenly p-1  d-flex align-items-center ">'+
+'                                    <label for="inputAccount" class="form-label">手動輪椅 : 新品價格 6666 元</label>'+
+'                                </div>'+  
+'                                <div class="col-12 justify-content-evenly p-1  d-flex align-items-center ">'+
+'                                    <label for="inputAccount" class="form-label">手動輪椅 : 新品價格 6666 元</label>'+
+'                               </div> '+ 
+'                                </div>'+
+
+                                
+'                                <div class="modal-footer d-flex justify-content-center align-items-center ">'+                                   
+'                                    <div class=" ">  '+                            
+'                                        <div class="button col-12 d-flex justify-content-center align-items-center ">'+
+'                                            <input type="button" class="btn btn-primary justify-content-center " value="先不用 !" onClick="">'+
+'                                            &nbsp&nbsp&nbsp&nbsp&nbsp'+
+'                                            <div class="button col-6 d-flex justify-content-center align-items-center ">'+
+'                                            <input type="button" class="btn btn-primary justify-content-center " value="前往購買" onClick="">'+
+'                                        </div>      '+   
+'                                    </div>'+
+'                                </div>'+
+'                            </form>  '+      
+'                        </div>   '+                                          
+'                </div>'+
+'        </div>'+
+'    </div>'
+;
+
+
+                                    
+                                
+
+            
+        
+ 
